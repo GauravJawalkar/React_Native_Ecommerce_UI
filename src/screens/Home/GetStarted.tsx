199136a22420935353611e8626b274db5c7adf86
Gaurav Jawalkar <gauravjawalkar8@gmail.com>
2025-09-28 21:53:03 +0530

    Learning devops for backend

--- src/screens/Home/GetStarted.tsx
+++ src/screens/Home/GetStarted.tsx
@@ -1,6 +1,6 @@
-import { View, Text, Button, TouchableOpacity, StatusBar } from 'react-native'
+import { View, Text, TouchableOpacity, StatusBar } from 'react-native'
 import React from 'react'
-import { Sprout, TreePalm } from 'lucide-react-native'
+import { Sprout } from 'lucide-react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { NativeStackScreenProps } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../../App'
@@ -20,7 +20,7 @@ const GetStartedScreen = ({ navigation }: GetStartedScreenProps) => {
                     <Sprout size={70} fill={"teal"} color={"white"} />
                     <Text className='text-4xl text-white font-bold'>Ecco_Marketplace</Text>
                 </View>
-                <TouchableOpacity onPress={() => { navigation.navigate("LoginScreen") }} activeOpacity={0.8} className='mb-10'>
+                <TouchableOpacity onPress={() => { navigation.navigate("LoginScreen") }} activeOpacity={0.8} className='mb-5'>
                     <Text className='capitalize font-semibold border text-green-800 bg-white py-3 px-4 rounded-lg text-center text-lg'>
                         Get Started
                     </Text>
